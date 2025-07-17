c5b3f9c90fa538cad5ade3c7b375bc05052a660e
Jake Swensen <jake@swensen.io>
2025-07-16 23:41:44 -0500

    fix: remove readme from rust doc

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#![doc = include_str!("../README.md")]
-
 #[cfg(feature = "cli-table")]
 use cli_table::{CellStruct, Color, Style, Table};
 use std::path::PathBuf;
